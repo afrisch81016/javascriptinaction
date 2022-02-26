8f3cb51225d3ea1033f98372b65e7c915cb4db07
Adam Frisch <afrisch88@outlook.com>
2022-02-26 16:56:18 -0600

    wrote for loops and researched many examples to determine the best way to code the logic

--- worksheet.js
+++ worksheet.js
@@ -1,51 +1,64 @@
-let dayOfWeek = "Monday";
-console.log(dayOfWeek);
+// let dayOfWeek = "Monday";
+// console.log(dayOfWeek);
 
-let dayOfWeek1 = "Friday";
-console.log(dayOfWeek1 + " I can't wait for Friday!");
+// let dayOfWeek1 = "Friday";
+// console.log(dayOfWeek1 + " I can't wait for Friday!");
 
-let animalInput = prompt("What is your favorite animal?");
-let colorInput = prompt("What is your favorite color?");
+// let animalInput = prompt("What is your favorite animal?");
+// let colorInput = prompt("What is your favorite color?");
 
-let responseofquestion = "I've never seen a " + colorInput + " " + animalInput + "!";
+// let responseofquestion = "I've never seen a " + colorInput + " " + animalInput + "!";
 
-console.log(responseofquestion)
+// console.log(responseofquestion)
 
-// #favorite breakfast is chicken and waffle#
-// #favorite lunch is Jimmy Johns#
-// #favorite dinner is Texas Roadhouse#
-
-
-let mealofchoicebreakfast = "Chicken and Waffles"
-let mealofchoicelunch = "Jimmy Johns"
-let mealofchoicedinner = "Teaxs Roadhouse"
-
-let userinputtime = prompt("What time of the day is it in military time? This will decide your choice of food to eat!");
-let timeOfDay = userinputtime
-
-if (timeOfDay <1200) {
-console.log (mealofchoicebreakfast)
-}
-else if (timeOfDay => 1200 || timeOfDay <=1700){
-console.log (mealofchoicelunch)
-
-} else if (timeOfDay =>1700){
-console.log (mealofchoicedinner)
-}
+// // #favorite breakfast is chicken and waffle#
+// // #favorite lunch is Jimmy Johns#
+// // #favorite dinner is Texas Roadhouse#
 
 
-const numbergenerated = Math.random() * (10-1) + 1
-console.log (numbergenerated)
+// let mealofchoicebreakfast = "Chicken and Waffles"
+// let mealofchoicelunch = "Jimmy Johns"
+// let mealofchoicedinner = "Teaxs Roadhouse"
 
-if (numbergenerated >0 || numbergenerated <2) {
-    console.log ("Beatles")
-}
-else if (numbergenerated >3 || numbergenerated <5) {
-console.log("Stones")
-}
-else if (numbergenerated >6|| numbergenerated <8) {
-    console.log("Floyd")
-}
-else if (numbergenerated = 9||10){
-    console.log("Hendrix")
-}
+// let userinputtime = prompt("What time of the day is it in military time? This will decide your choice of food to eat!");
+// let timeOfDay = userinputtime
+
+// if (timeOfDay <1200) {
+// console.log (mealofchoicebreakfast)
+// }
+// else if (timeOfDay => 1200 || timeOfDay <=1700){
+// console.log (mealofchoicelunch)
+
+// } else if (timeOfDay =>1700){
+// console.log (mealofchoicedinner)
+// }
+
+
+// const numbergenerated = Math.random() * (10-1) + 1
+// console.log (numbergenerated)
+
+// if (numbergenerated == 0 || numbergenerated <2) {
+//     console.log ("Beatles")
+// }
+// else if (numbergenerated >3 || numbergenerated <5) {
+// console.log("Stones")
+// }
+// else if (numbergenerated >6|| numbergenerated <8) {
+//     console.log("Floyd")
+// }
+// else if (numbergenerated >9|| numbergenerated == 10){
+//     console.log("Hendrix")
+// }
+
+
+// let loop = "JavaScript is cool";
+// for(let message = 0; message < 8; message += 1) {
+//   loop +=  "JavaScript is cool ";
+// }
+// console.log(loop);
+
+// let numberprintedonconsole = "";
+// for(let number = 1; number < 11; number += 1) {
+//   numberprintedonconsole += number + " ";
+// }
+// console.log(numberprintedonconsole);
